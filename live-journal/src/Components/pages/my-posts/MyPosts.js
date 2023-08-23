7557361172ef50a17c0d642f6eeb06d333ae5b33
DmitryMolokanov <molod1690@yandex.ru>
2023-08-23 21:57:02 +0300

    added function edit and styles

--- live-journal/src/Components/pages/my-posts/MyPosts.js
+++ live-journal/src/Components/pages/my-posts/MyPosts.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MainMenu from "../../header/header/MainMenu";
 import "../../styles/my-posts/myPosts.css";
 
@@ -7,6 +7,8 @@ function MyPosts() {
   const login = JSON.parse(userData);
   const [isData, setIsData] = useState(false);
   const [posts, setPosts] = useState();
+  const [editForm, setEditForm] = useState(false);
+  const editPostForm = useRef();
 
   useEffect(() => {
     fetch("/my_posts", {
@@ -24,21 +26,56 @@ function MyPosts() {
       .catch((err) => console.log(`Error: ${err}`));
   }, []);
 
+  // функция вызывает окно для исправления поста
+
+  function editText(event) {
+    const id = event.target.firstChild.innerHTML;
+    const post = posts.filter((p) => p._id === id);
+    editPostForm.current.value = post[0].text;
+    setEditForm(true);
+  }
+
+  // функция закрывает окно для исправления поста
+  function closeForm() {
+    setEditForm(false);
+  }
+
   return (
     <div>
       <MainMenu />
       <div className="all-blok">
+        <form
+          className="form-edit"
+          style={
+            editForm
+              ? { display: "flex", flexDirection: "column" }
+              : { display: "none" }
+          }
+        >
+          <button className="close-form-edit" onClick={closeForm}>
+            X
+          </button>
+          <textarea ref={editPostForm} cols="50" rows="20"></textarea>
+          <button className="form-edit-btn">Edit</button>
+        </form>
+
         <div className="blok-for-posts">
-          {isData
-            ? posts.map((item) => {
-                return (
-                  <div className="my-posts-blok">
-                    <div className="my-posts-text">{item.text}</div>
-                    <div className="date">{item.date}</div>
-                  </div>
-                );
-              })
-            : "Error"}
+          {isData ? (
+            posts.map((item, index) => {
+              return (
+                <div className="my-posts-blok" key={index}>
+                  <button className="btn-edit-post" onClick={editText}>
+                    <div style={{ display: "none" }}>{item._id}</div>
+                    Edit post
+                  </button>
+                  <div className="my-posts-text">{item.text}</div>
+                  <div className="date">{item.date}</div>
+                </div>
+              );
+            })
+          ) : (
+            <h1>You haven't posted any yet</h1>
+          )}
         </div>
       </div>
     </div>
